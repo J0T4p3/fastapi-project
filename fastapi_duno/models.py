888cefd8d6b7bf529553b4f137564f7bf4540afc
reversejp <joaopedrosilvadelima@gmail.com>
2026-02-10 18:51:58 -0300

    feat(users): added updated_at to users table

--- fastapi_duno/models.py
+++ fastapi_duno/models.py
@@ -9,10 +9,16 @@ table_registry = registry()
 @mapped_as_dataclass(table_registry)
 class User:
     __tablename__ = 'users'
+
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
     email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),
+        onupdate=func.now(),
     )
