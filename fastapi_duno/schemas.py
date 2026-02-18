0f87a6392c9eaf18eec02c10179b7ff0c9436838
reversejp <joaopedrosilvadelima@gmail.com>
2026-02-18 11:22:58 -0300

    feat(tests): removed unused schemas, added message

--- fastapi_duno/schemas.py
+++ fastapi_duno/schemas.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
 
 
+class Message(BaseModel):
+    message: str
+
+
 class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UserPublic(BaseModel):
@@ -12,10 +17,6 @@ class UserPublic(BaseModel):
     username: str
     email: EmailStr
     model_config = ConfigDict(from_attributes=True)
-
-
-class UserDB(UserSchema):
-    id: int
 
 
 class Userlist(BaseModel):
